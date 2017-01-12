4d10682ba792a1b4b8140308b3646bdb5b7e401b
abinesh1006 <abineshsiva191@gmail.com>
2017-01-12 15:56:54 +0530

    Create pascal.c

---
+++ pascal.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    int i,j;
+    int a[100][100]={0};
+    scanf("%d",&n);
+    for (i=1;i<=n;i++)
+    {
+        for (j=0;j<i;j++)
+        {
+            if(j==0 || j==n-1 )
+            {
+                a[i][j]=1;
+            }
+            else
+            {
+                a[i][j]=a[i-1][j-1]+a[i-1][j];
+            }
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
