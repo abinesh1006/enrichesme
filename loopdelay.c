414a3d93e7abd87bbda21a018f5b5e6c5142def0
abinesh1006 <abineshsiva191@gmail.com>
2017-01-15 13:28:22 +0530

    Create loopdelay.c

---
+++ loopdelay.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char s[]="HAPPY PONGAL";
+    int i,j,k;
+    while(1)
+    {
+        for (i=0;i<strlen(s);i++)
+        {
+            printf("%c",s[i]);
+            for(j=0;j<=10000;j++)
+            {
+                for(k=0;k<2000;k++)
+                {
+                }
+            }
+        }
+        system("cls");
+
+    }
+
+}
