cc7dc5254680c3ae8b061955dbdf64589549c0a9
Abinesh <abineshsiva191@gmail.com>
2017-04-26 10:59:31 +0530

    Create p.bettdicroll.c

---
+++ p.bettdicroll.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    char b;
+    int arr[100];
+    scanf("%d",&arr[0]);
+    int i;
+    scanf("%c",&b);
+     i=0;
+     int x,y;
+    while(b!='\n')
+    {
+        scanf("%d",&arr[++i]);
+        scanf("%c",&b);
+    }
+     x=arr[i-1];
+     y=arr[i];
+    printf("%d%d",x,y);
+    i=i-2;
+    int j;
+    int sum=0;
+    for(j=0;j<=i;j++)
+    {
+        if(arr[j]%2==0)
+        {
+            sum=sum+x;
+        }
+        else{
+            sum=sum-y;
+        }
+    }
+    printf("%d",sum);
+
+
+}
