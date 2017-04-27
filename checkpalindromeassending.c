c5b518563397f3b74ca0d6eff5f375de5187374a
Abinesh <abineshsiva191@gmail.com>
2017-04-27 21:03:22 +0530

    Update and rename check.c to checkpalindromeassending.c

--- checkpalindromeassending.c
+++ checkpalindromeassending.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
 #include <stdlib.h>
+#include<string.h>
 
 int main()
 {
     int n;
-    char min;
     scanf("%d",&n);
+    int finalcount=0;
     int i;
     char a[11][100];
+    char final[1000000]="";
+    char *p=final;
     char res[1000000]="";
+    char min;
 
     for(i=0;i<n;i++)
     {
@@ -16,6 +20,7 @@ int main()
         strcat(res,a[i]);    
     }
     int j;
+    min=res[0];
     int len=strlen(res);
     for(i=0;i<len;i++)
     {
@@ -30,9 +35,9 @@ int main()
             }
         }
     }
-    printf("%s\n",res);
-    printf("\n%d\n",len);
+  
     int count;
+    int k=0;
     for(i=0;i<len;i++)
     {
         count=0;
@@ -44,21 +49,25 @@ int main()
                 i++;
                 if(count%2!=0)
                 {
-                    printf("%c",res[i]);
-                }
-            
-            }
-            if(count%2==0)
-            {
-                min=res[i];
-                if(min>res[i])
-                {
-                    min=res[i];
+                    finalcount++;
+                    final[k++]=res[i];
+                    
+                  
                 }
             }
             
         }
+        if(count%2==0)
+        {
+            if(min>res[i])
+            {
+                min=res[i];
+            }
+        }
     }
+    final[k]='\0';
+    printf("%s",p);
     printf("%c",min);
+    printf("%s",strrev(final));
     
 }
