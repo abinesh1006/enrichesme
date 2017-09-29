99c8f42fc2b9751f46cba512cebb903077b300c1
Abinesh <abineshsiva191@gmail.com>
2017-09-29 22:14:59 +0530

    Create studentz_dep.c

---
+++ studentz_dep.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int a;
+	scanf("%d",&a);
+	char name[100][100];
+	char dep[100][100];
+	int i;
+	for(i=0;i<a;i++)
+	{
+	    scanf("%s",name[i]);
+	    scanf("%s",dep[i]);
+	}
+	char fin[100];
+	scanf("%s",fin);
+	for(i=0;i<a;i++)
+	{
+	    if(strcmp(fin,dep[i])==0)
+	    {
+	        printf("%s\n",name[i]);
+	    }
+	}
+	return 0;
+}
