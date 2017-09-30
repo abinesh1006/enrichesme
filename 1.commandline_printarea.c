2544371e81239f605607b94c86ef9d759ec338c7
Abinesh <abineshsiva191@gmail.com>
2017-09-30 09:53:08 +0530

    Update 1.commandline_printarea.c

--- 1.commandline_printarea.c
+++ 1.commandline_printarea.c
@@ -1,1 +1,11 @@
-
+#include<stdio.h>
+#include<math.h>
+int main(int argc,char *argv[])
+{
+    float x;
+    sscanf(argv[1],"%f",&x);
+    float di=x/2;
+    float ans=22*pow(di,2.0)/7.0;
+    printf("%.2f",ans);
+    return 0;
+}
