33ff4174eb7018884118d1333ca97aad010f4548
Abinesh <abineshsiva191@gmail.com>
2017-10-05 19:54:44 +0530

    Update SecndBgEvn.c

--- SecndBgEvn.c
+++ SecndBgEvn.c
@@ -31,7 +31,7 @@ int main()
             max2=max1;
             max1=a[i];
         }
-        else if(max2<a[i])
+        else if(max2<a[i] && a[i]!=max1)
         {
            max2=a[i];
         }
