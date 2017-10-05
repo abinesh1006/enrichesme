9661774a8fd0ca4b756844b16bc04c90455e296b
Abinesh <abineshsiva191@gmail.com>
2017-10-05 20:02:36 +0530

    Create secondlargest.c

---
+++ secondlargest.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(int a,char **b)
+{
+    int i,j,max1,max2;
+    int arr[1000];
+    for(i=1;i<a;i++)
+    {
+        arr[i]=atoi(b[i]);
+    }
+    max1=-1;
+    max2=-1;
+    for(i=1;i<a;i++)
+    {
+        if(max1<arr[i])
+        {
+            max2=max1;
+            max1=arr[i];
+         }
+         else if(max2<arr[i] && arr[i]!=max1)
+         {
+             max2=arr[i];
+         }
+      
+    }
+    printf("%d",max2);    
+  
+    
+}
