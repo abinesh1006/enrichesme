6d0447ab82f4cb3c7b2dd875663691297a57e298
Abinesh <abineshsiva191@gmail.com>
2017-10-15 21:14:47 +0530

    Create imediateprime.c

---
+++ imediateprime.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int fla,i,j,a,aa[100000];
+  scanf("%d",&a);
+  for(i=0;i<a;i++)
+  {
+      scanf("%d",&aa[i]);
+  }
+  
+  for(i=1;i<a;i++)
+  {
+      fla=0;
+      for(j=i-1;j>=0;j--)
+      {
+          if(aa[i]<aa[j])
+          {
+              printf("%d ",aa[j]);
+              fla=1;
+              break;
+          }
+          
+      }
+      if(fla==0)
+      {
+          printf("%d ",0);
+      }
+  }
+
+}
