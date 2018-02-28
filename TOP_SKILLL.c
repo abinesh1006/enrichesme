94fb0afc8a9bbcbe55ff2599e9c549f872e53fbd
Abinesh <abineshsiva191@gmail.com>
2018-02-28 22:41:37 +0530

    Update TOP_SKILLL.c

--- TOP_SKILLL.c
+++ TOP_SKILLL.c
@@ -8,65 +8,54 @@ int main()
    int i,j,s=0,test[20],odi[20],t20[20];
    char qw[20][20000];
    char name[20][20000];
-   int sum[2000]={0};
+   int sum[20000]={0};
    for(i=0;i<a;i++)
    {
-       gets(qw[i]);
-   }
-   int k=0;
-   for(i=0;i<a;i++)
-   {
-       sscanf(qw[i],"%s %d %d %d",name[i],&test[i],&odi[i],&t20[i]);
+       scanf("%s%d%d%d",name[i],&test[i],&odi[i],&t20[i]);
        sum[i]=test[i]+odi[i]+t20[i];
+         
    }
-   char max[10000];
-   int mx=0;
-   int mxin=0;
-   int maxtest=0,maxodi=0,maxt20=0;
+   char max[1000];
+   int mx=0,maxtest=0,maxodi=0,maxt20=0;
    for(i=0;i<a;i++)
    {
-    if(mx<sum[i])
-      {
-        strcpy(max,name[i]);
-        mx=sum[i];
-        maxtest=test[i];
-        maxodi=odi[i];
-         maxt20=t20[i];
-      }
-      else if (mx==sum[i])
-      {
-          if(maxtest<test[i])
-          {
-              strcpy(max,name[i]);
-              mx=sum[i];
-              maxtest=test[i];
-              maxodi=odi[i];
+       if(mx<sum[i])
+       {
+           strcpy(max,name[i]);
+           mx=sum[i];
+           maxtest=test[i];
+           maxodi=odi[i];
+           maxt20=t20[i];
+       }
+       else if (mx==sum[i])
+       {
+           if(maxtest<test[i])
+           {
+               strcpy(max,name[i]);
+               maxtest=test[i];
+               maxodi=odi[i];
                maxt20=t20[i];
-            }
-            else if(maxtest==test[i])
-            {
-                if(maxodi<odi[i])
-                {
-                    strcpy(max,name[i]);
-                    maxodi=odi[i];
-                     maxt20=t20[i];
-                }
-                else if (maxodi==odi[i])
-                {
-                    if(maxt20<t20[i])
-                    {
-                        strcpy(max,name[i]);
-                        maxt20=t20[i];
-                    }
-                }
-
-            }
-
-          }
-
-      }
-
-
-printf("%s",max);
+           }
+           else if(maxtest==test[i])
+           {
+               if(maxodi<odi[i])
+               {
+                   strcpy(max,name[i]);
+                   maxodi=odi[i];
+                   maxt20=t20[i];
+                   
+               }
+               else if (maxodi==odi[i])
+               {
+                   if(maxt20<t20[i])
+                   {
+                       strcpy(max,name[i]);
+                       maxt20=t20[i];
+                   }
+               }
+           }
+       }
+   }
+   printf("%s",max);
 
 }
